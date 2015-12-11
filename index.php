10b099abf6f672c1abf520771101cd1161e82914
Alexandre Simonet <contact@alexandresimonet.fr>
2015-12-11 14:33:36 +0100

    Fix connection login

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 require 'vendor/autoload.php';
 
-$Connection = new \ORM\Connection('localhost', 'orm', 'root', 'abx24z4hb9zX');
+$Connection = new \ORM\Connection('', '', '', '');
 $EntityManager = new \ORM\Entity\Manager();
 
 $Article = new \Entity\Article();
